1eb84f9e11e88c306460d8ff29e6db50380bdbe8
ansible447karthik <135459737+ansible447karthik@users.noreply.github.com>
2024-05-27 13:32:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,14 @@
-provider "aws"{}
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "ansible_node" {
+  count         = 5
+  ami           = "ami-0ac67a26390dc374d"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-046ea3fb42c11e916"
+  key_name      = "EU_WEST_WebSvcsDev"
+  tags = {
+    ManagedBy = "Ansible"
+  }
+}
